4284140b3a1a0413276051786d14cef25fb8a61b
ParulGupta-creater <pgoel01@gmail.com>
2025-07-08 20:59:49 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y wget unzip gnupg curl
+# Install dependencies
+apt-get update && apt-get install -y wget unzip curl gnupg2 ca-certificates
 
-# Install Google Chrome for headless browsing
+# Install Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb -y
+apt install -y ./google-chrome-stable_current_amd64.deb || apt-get -f install -y
+
+# Ensure Chrome installed
+google-chrome --version
+
+# Install pip dependencies
+pip install -r requirements.txt
+
