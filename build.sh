38627977a0d1300f3444d04ef5443d2d94f93355
ParulGupta-creater <pgoel01@gmail.com>
2025-07-10 13:07:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
-
-# Install dependencies
-apt-get update && apt-get install -y wget unzip curl gnupg2 ca-certificates
-
-# Install Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb || apt-get -f install -y
-
-# Ensure Chrome installed
-google-chrome --version
-
-# Install pip dependencies
 pip install -r requirements.txt
-python --version
+playwright install --with-deps
